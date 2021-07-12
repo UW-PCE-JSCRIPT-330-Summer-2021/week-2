ebc6c6c084c54529371bc1b4ad93346fd4cfd300
Tamara Trefilova <tamara.trefilova@gmail.com>
2021-07-11 17:09:43 -0700

    homework finished

--- daos/calendars.js
+++ daos/calendars.js
@@ -1,9 +1,16 @@
 const Calendars = require('../models/calendars');
 
+const Events = require('../models/events');
+
 module.exports = {};
   
 module.exports.create = async (name) => {
-  return await Calendars.create({ name });
+  try {
+    return await Calendars.create(name);
+  }catch (e) {
+    return null;
+  }
+  
 };
 
 module.exports.getById = async (id) => {
@@ -15,11 +22,112 @@ module.exports.getById = async (id) => {
   }
 };
 
-module.exports.updateById = async (id, newData) => {
+module.exports.updateById = async (id, newName) => {
   try {
-    const calendar = await Calendars.findOneAndUpdate({ _id: id }, newData, { new: true }).lean();
+    const calendar = await Calendars.findOneAndUpdate({ _id: id }, {$set:{name:newName}}, { new: true }).lean();
     return calendar;
   } catch (e) {
     return null;
   }
+};
+
+module.exports.getAll = async () => {
+  try {
+    const calendar = await Calendars.find({});
+    return calendar;
+  } catch (e) {
+    return null;
+  }
+};
+
+
+
+module.exports.removeById = async (id) => {
+
+  try {
+ 
+    const rez = await Calendars.remove({ _id: id });
+    return rez;
+
+  } catch (e) {
+    return null;
+  }
+ 
+};
+
+
+
+module.exports.getEvents = async ( id) => {
+  try {
+    const calendar = await Calendars.findOne({ _id: id });
+    if(calendar){
+      const calEvents = await Events.find({calendarId: id});
+      if(calEvents){
+        return calEvents;
+      }
+      return null;
+    } else {
+      return null;
+    }
+   
+  } catch (e) {
+    return null;
+  }
+};
+
+
+
+module.exports.getEventById = async ( eventId) => {
+  try {
+    const event = await Events.findOne({ _id: eventId}).lean();
+    return event;
+  } catch (e) {
+    return null;
+  }
+};
+
+
+
+module.exports.createEvent = async (calId, event) => {
+  try {
+  
+    const calendar = await Calendars.findOne({ _id: calId });
+    if(calendar){
+      const id = calendar._id;
+      event.calendarId = calendar._id;
+      const events = await Events.create(event);
+   
+      return events;
+    }
+
+    return null;    
+  } catch (e) {
+    return null;
+  }
+  
+};
+
+
+module.exports.updateEvent = async (id, newDate, newName) => {
+  try {
+  
+    const event = await Events.findOneAndUpdate({ _id: id }, {$set:{date:newDate, name:newName }}, { new: true }).lean();
+    return event;
+  } catch (e) {
+    return null;
+  }
+};
+
+
+module.exports.deleteById = async (id) => {
+
+  try {
+ 
+    const rez = await Events.remove({ _id: id });
+    return rez;
+
+  } catch (e) {
+    return null;
+  }
+ 
 };
--- models/calendars.js
+++ models/calendars.js
@@ -1,8 +1,23 @@
 const mongoose = require('mongoose');
 
-const calendarSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-});
 
+// const eventSchema = new mongoose.Schema({
+//   name: { type: String, required: true },
+//   date: {type: Date, requred: true},
+//   calendarId: {type: String, requred: true} 
+// });
+
+
+// const calendarSchema = new mongoose.Schema({
+//   name: { type: String, required: true },
+//   events: { type: [eventSchema] }
+//   },
+//     { strict: false });
+
+
+const calendarSchema = new mongoose.Schema({
+  name: { type: String, required: true }
+  },
+    { strict: false });
 
 module.exports = mongoose.model("calendars", calendarSchema);
