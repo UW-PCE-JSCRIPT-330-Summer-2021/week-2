57a6e90133496b162538f21f7166e88e7bdd5497
JS-Q <76979814+JS-Q@users.noreply.github.com>
2021-07-20 01:42:58 -0500

    Commit#1

--- daos/calendars.js
+++ daos/calendars.js
@@ -2,10 +2,19 @@ const Calendars = require('../models/calendars');
 
 module.exports = {};
   
-module.exports.create = async (name) => {
-  return await Calendars.create({ name });
-};
+module.exports.create = async (name ) => {
+  return await Calendars.create( name );
 
+}; 
+// Get all
+module.exports.getAll = async () => {
+  try{
+    return await Calendars.find({}).lean
+ } catch (e) {
+  return null;
+ }
+};
+//Get by ID
 module.exports.getById = async (id) => {
   try {
     const calendar = await Calendars.findOne({ _id: id }).lean();
@@ -14,11 +23,19 @@ module.exports.getById = async (id) => {
     return null;
   }
 };
-
+//Update by ID
 module.exports.updateById = async (id, newData) => {
   try {
     const calendar = await Calendars.findOneAndUpdate({ _id: id }, newData, { new: true }).lean();
     return calendar;
+  } catch (e) {
+    return null;
+  }
+};
+//Remove by ID
+module.exports.removebyId = async (id) => {
+  try {
+    return await Calendars.deleteOne({ _id: id});
   } catch (e) {
     return null;
   }
---
+++ daos/events.js
@@ -0,0 +1,40 @@
+const Events = require ('../models/events');
+module.exports={};
+
+module.exports.create = async (event) => {
+    return await Events.create(event);
+};
+//getall
+module.exports.getAll = async(calendarId) => {
+        try{
+            return await Events.find({calendarId}).lean();
+        } catch (e) {
+            return null;
+        }
+};
+//getbyID
+module.exports.getById = async (id) => {
+    try {
+        const event = await Events.findOne ({ _id: id }).lean();
+        return event;
+    } catch (e) {
+        return null;
+    }
+};
+//UpdatebyId
+module.exports.updateById = async (id, newData) => {
+    try {
+        const event = await Events.findOneAndUpdate({ _id: id }, newData, { new: true }).lean();
+        return event;
+
+    } catch (e) {
+        return null;
+    }
+};
+module.exports.removebyId = async (id) => {
+    try{
+        return await Events.deleteOne({ _id:id});
+    } catch (e) {
+        return null;
+    }
+};
