d1625b78ef631c1d86becceebd24b452ceead378
Patrick Gronstal <gronstal@uw.edu>
2021-08-26 12:24:55 -0600

    Incomplete but updated

--- routes/calendars.js
+++ routes/calendars.js
@@ -1,14 +1,12 @@
 const { Router } = require("express");
-
 const CalendarDAO = require('../daos/calendars');
-
 const router = Router();
 
 router.get("/", async (req, res, next) => {
   try {
-    const calendars = await CalendarDAO.getAll();
-    res.json(calendars);
-  } catch(e) {
+      const calendars = await CalendarDAO.getAllCalendars();
+      res.json(calendars);
+    } catch(e) {
     next(e);
   }
 });
@@ -16,7 +14,7 @@ router.get("/", async (req, res, next) => {
 router.get("/:id", async (req, res, next) => {
   try {
     const calendar = await CalendarDAO.getById(req.params.id);
-    if (calendar) {
+    if (calendar ) {
       res.json(calendar);
     } else {
       res.sendStatus(404);
@@ -28,14 +26,46 @@ router.get("/:id", async (req, res, next) => {
 
 router.delete("/:id", async (req, res, next) => {
   try {
-    const calendar = await CalendarDAO.removeById(req.params.id);
+    const calendar = await CalendarDAO.getById(req.params.id);
     if (calendar) {
       res.sendStatus(200);
+      return await CalendarDAO.deleteById(req.params.id);
     } else {
       res.sendStatus(404);
     }
   } catch(e) {
     next(e);
+  }
+});
+
+router.put("/:id", async(req,res,next) => {
+  try{
+    const calendar = await CalendarDAO.getById(req.params.id);
+    calendarName = req.body.name;
+    if(calendar){
+      updateCalendar = await CalendarDAO.updateById(req.params.id, req.body.name);
+      if(updateCalendar.body !=null && updateCalendar.body.name !=="{}"){res.json(updateCalendar);}
+      else{return res.sendStatus(400)}
+      next();
+    } else if(req.body.name ==null ){
+      return res.sendStatus(400);
+    } else return res.sendStatus(404);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/", async (req, res, next)=>{
+  try{
+    const calendarName = req.body.name;
+    if(calendarName){
+      res.sendStatus(200);
+      return await CalendarDAO.create(req.body);
+    } else{
+      return res.sendStatus(400).send("Name is Missing");
+    }
+  } catch(e) {
+
   }
 });
 
